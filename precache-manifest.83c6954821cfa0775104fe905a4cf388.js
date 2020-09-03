690295e2bd5be90db1afc14c96b3cf40394b2047
Marcio <marcio@draimo.com>
2020-09-02 21:54:23 -0300

    Updates

---
+++ precache-manifest.83c6954821cfa0775104fe905a4cf388.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070b3006ba5f9b39dbf80831f2491edb",
+    "url": "/REACT-hook-crud-pestanias/index.html"
+  },
+  {
+    "revision": "b8dec22fde58a93126ac",
+    "url": "/REACT-hook-crud-pestanias/static/css/main.1ea84cde.chunk.css"
+  },
+  {
+    "revision": "585b854d91d70fc343a5",
+    "url": "/REACT-hook-crud-pestanias/static/js/2.13222083.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/REACT-hook-crud-pestanias/static/js/2.13222083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8dec22fde58a93126ac",
+    "url": "/REACT-hook-crud-pestanias/static/js/main.5b61f0cf.chunk.js"
+  },
+  {
+    "revision": "3a867ffbc08650bba916",
+    "url": "/REACT-hook-crud-pestanias/static/js/runtime-main.a03fa7c6.js"
+  }
+]);
